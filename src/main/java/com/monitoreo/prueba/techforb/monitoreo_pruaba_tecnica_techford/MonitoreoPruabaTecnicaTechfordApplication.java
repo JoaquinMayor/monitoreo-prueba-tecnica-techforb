2829468aac718744cd669d5f2f757c40318927b2
JoaquinMayor <joaquin88mayor@gmail.com>
2024-06-23 16:23:46 -0300

    deploy

--- src/main/java/com/monitoreo/prueba/techforb/monitoreo_pruaba_tecnica_techford/MonitoreoPruabaTecnicaTechfordApplication.java
+++ src/main/java/com/monitoreo/prueba/techforb/monitoreo_pruaba_tecnica_techford/MonitoreoPruabaTecnicaTechfordApplication.java
@@ -1,13 +1,50 @@
 package com.monitoreo.prueba.techforb.monitoreo_pruaba_tecnica_techford;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MonitoreoPruabaTecnicaTechfordApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonitoreoPruabaTecnicaTechfordApplication.class, args);
+		
 	}
 
+	@Bean
+    public ServletWebServerFactory servletContainer() {
+        // Enable SSL/TLS
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // Configure HTTPS
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+        return tomcat;
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        return connector;
+    }
 }
+
+
